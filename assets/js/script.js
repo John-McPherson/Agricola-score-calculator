dce92a117c6cc43b53fb8de53110e956c4c8bb7d
John-McPherson <john-mac1989@hotmail.co.uk>
2022-01-07 19:40:17 +0000

    add new screen function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,19 @@ function addEventListeners() {
     for (let x = 0; x < plus.length; x++) {
         plus[x].addEventListener("click", increase);
         minus[x].addEventListener("click", increase);
-        next[x].addEventListener("click", set_up);
+        if (round === 0) {
+            next[x].addEventListener("click", set_up);
+        } else {
+            next[x].addEventListener("click", newScreen);
+        }
     }
 }
 let input = 3;
-let player_count = 3;
+let playerCount = 3;
+let currentPlayer = 0;
 let players = [];
-type = ["Fields", "pastures"]
+let type = ["", "Fields", "Pastures"]
+let round = 0;
 
 function increase() {
     if (this.className === "plus" && input < 5) {
@@ -29,7 +35,7 @@ function update_input() {
 
 function set_up() {
     let output = document.getElementsByClassName("input-text")[0]
-    player_count = parseInt(output.innerHTML);
+    playerCount = parseInt(output.innerHTML);
     newPlayer(1)
 
 }
@@ -40,8 +46,8 @@ function nextScreen() {
     let setupArea = document.getElementById("set-up")
     setupArea.innerHTML = `
     <h1>Agricola Score Calculator</h1>
-    <h2>${players[0].playerName}</h2>
-    <p>How Many ${type[0]}?</p>
+    <h2>${players[currentPlayer].playerName}</h2>
+    <p>How Many ${type[round]}?</p>
     <p class="input-text">0</p>
     <span class="plus-minus">
         <button class="minus">-</button>
@@ -53,6 +59,23 @@ function nextScreen() {
     </span>
     `
     addEventListeners()
+
+}
+
+function newScreen() {
+    if (currentPlayer !== playerCount - 1) {
+        currentPlayer++
+        nextScreen()
+    } else {
+        round++
+        console.log(round)
+        currentPlayer = 0;
+        nextScreen()
+
+
+    }
+
+
 }
 
 function updatePlayer(event) {
@@ -62,13 +85,11 @@ function updatePlayer(event) {
         playerName: name
     }
     players.push(player)
-    console.log(players)
     newPlayer(players.length + 1)
 }
 
 function newPlayer(y) {
-    if (y <= player_count) {
-        console.log(y)
+    if (y <= playerCount) {
         let setup_area = document.getElementById("set-up")
         setup_area.innerHTML = `<h1>Agricola Score Calculator</h1> 
     <form onsubmit="updatePlayer(event)">
@@ -80,6 +101,7 @@ function newPlayer(y) {
 </form>
     `
     } else {
+        round++
         nextScreen()
 
     }
